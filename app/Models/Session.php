a2f738701c440204a0c5b093cb44cdf4138fe607
Francis <brakay1136@gmail.com>
2026-06-12 08:18:12 -0800

    added a session controller

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
-    //
+    protected $fillable = [
+        'task_id',
+        'start_time',
+        'end_time',
+        'duration_seconds'
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        
+    ];
+
+    public function sessions()
+    {
+        return $this->hasMany(Session::class);
+    }
 }
